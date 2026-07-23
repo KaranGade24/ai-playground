838439e298715eb9e69fd862a8effb144f23c7d2
KaranGade24 <gadekaran24@gmail.com>
2026-07-23 21:43:34 +0530

    Initial commit

--- config/geminiConfig.js
+++
@@ -1,9 +0,0 @@
-import { GoogleGenAI } from "@google/genai";
-import dotenv from "dotenv";
-dotenv.config();
-
-const apiKey = process.env.GOOGLE_API_KEY;
-
-export const AI = new GoogleGenAI({
-  apiKey: apiKey,
-});
